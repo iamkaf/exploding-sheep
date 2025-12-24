b9df33041bd19716905d8bbc038af1caf9dfe28f
Kaf <coffeechopdev@gmail.com>
2025-12-24 00:31:57 -0300

    Initial commit: Exploding Sheep v1.0.0

---
+++ forge/src/main/java/com/iamkaf/explodingsheep/ExplodingSheepForge.java
@@ -0,0 +1,13 @@
+package com.iamkaf.explodingsheep;
+
+import com.iamkaf.explodingsheep.ExplodingSheepConstants;
+import com.iamkaf.explodingsheep.ExplodingSheepMod;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ExplodingSheepConstants.MOD_ID)
+public class ExplodingSheepForge {
+
+    public ExplodingSheepForge() {
+        ExplodingSheepMod.init();
+    }
+}
